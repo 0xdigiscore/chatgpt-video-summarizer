6bcdcc6941cd8799598d5e51bc53429a52a1920b
0xhelloweb3 <1245816264@qq.com>
2023-03-16 16:15:59 +0800

    feat: add default api key

--- src/options/options.js
+++ src/options/options.js
@@ -9,7 +9,7 @@ document.getElementById('save').addEventListener('click', () => {
 
 chrome.storage.sync.get(['api_key', 'max_tokens'], (data) => {
   document.getElementById('max_tokens').value = data.max_tokens || 150;
-  document.getElementById('api_key').value = data.api_key || '';
+  document.getElementById('api_key').value = data.api_key || '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';
 });
 
 localizeHtmlPage();
